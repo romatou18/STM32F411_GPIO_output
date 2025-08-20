c738539f829542c05974d50c2033afe170707065
Romain <romatou18@hotmail.com>
2025-08-21 10:59:47 +1200

    syscfg h

---
+++ syscfg.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define SYSCFG_ADR 0x40013800
+#define EXTI_ADDRESS 0x40013C00
+
+//typedef struct
+//{
+//  __IO uint32_t MEMRMP;       /*!< SYSCFG memory remap register,                      Address offset: 0x00      */
+ // __IO uint32_t PMC;          /*!< SYSCFG peripheral mode configuration register,     Address offset: 0x04      */
+//  __IO uint32_t EXTICR[4];    /*!< SYSCFG external interrupt configuration registers, Address offset: 0x08-0x14 */
+//  uint32_t      RESERVED[2];  /*!< Reserved, 0x18-0x1C                                                          */
+ // __IO uint32_t CMPCR;        /*!< SYSCFG Compensation cell control register,         Address offset: 0x20      */
+//} SYSCFG_TypeDef;
+
+
+//typedef struct
+//{
+ // __IO uint32_t IMR;    /*!< EXTI Interrupt mask register,            Address offset: 0x00 */
+//  __IO uint32_t EMR;    /*!< EXTI Event mask register,                Address offset: 0x04 */
+//  __IO uint32_t RTSR;   /*!< EXTI Rising trigger selection register,  Address offset: 0x08 */
+ // __IO uint32_t FTSR;   /*!< EXTI Falling trigger selection register, Address offset: 0x0C */
+ // __IO uint32_t SWIER;  /*!< EXTI Software interrupt event register,  Address offset: 0x10 */
+//  __IO uint32_t PR;     /*!< EXTI Pending register,                   Address offset: 0x14 */
+//} EXTI_TypeDef;
+
